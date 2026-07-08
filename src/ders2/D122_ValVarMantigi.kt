1e3ff6646efb323bee43c60900977f304aeb6845
Gürkay Birinci <gurkaybirinci@gmail.com>
2026-07-08 13:15:05 +0300

    Ders2: Değişkenler ve Tipler

---
+++ src/ders2/D122_ValVarMantigi.kt
@@ -0,0 +1,14 @@
+package ders2
+
+fun main() {
+    val uygulamaAdi = "Hava Durumu Pro"
+    // uygulamaAdi = "Hava Durumu Pro v2" (Hata alırız. Çünkü val olan bir değişkene yeniden değer atanamaz!)
+
+    var indirimOrani = 10
+    indirimOrani = 15 // var türünde bir değişken olduğu için, değeri değiştirebilirim.
+
+    println("Uygulama Adı: " + uygulamaAdi)
+    println("İndirim Oranı: " + indirimOrani)
+
+
+}
---
+++ src/ders2/D123_TipCikarimi.kt
@@ -0,0 +1,15 @@
+package ders2
+
+fun main() {
+    val sehir: String = "Ankara"
+    val sicaklik = 15
+    val nemOrani = 0.45
+    val dogruMu = true
+
+    var sistemMesaji: String
+    sistemMesaji = "Sıcaklık, uyarı seviyesinde!"
+
+    println("Mesaj: " + sistemMesaji)
+
+
+}
